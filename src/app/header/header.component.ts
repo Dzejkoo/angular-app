21600d29505322a062a57c6abc917236bfc2ee6c
Jakub Jóźwiak <jozwiak@primesoft.local>
2022-12-19 13:03:59 +0100

    I created basic navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
+  @Output() page: EventEmitter<string> =  new EventEmitter()
 
+  currentPage(value: string){
+    this.page.emit(value)
+  }
 }
